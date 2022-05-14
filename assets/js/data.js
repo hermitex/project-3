e0a5820c484352f57b051cc93c9e6e97daaa858a
hermitex <mercytibs@yahoo.com>
2022-05-14 12:46:12 +0300

    add quiz database

---
+++ assets/js/data.js
@@ -0,0 +1,44 @@
+export const quizes = [
+  {
+    question: "HTML can be used to build a website on its own",
+    answer: true,
+  },
+  {
+    question: "HTML is the only markup language in existence",
+    answer: false,
+  },
+  {
+    question:
+      "There is no difference between block elements and inline elements",
+    answer: false,
+  },
+  {
+    question: "CSS can be used as an alternative of HTML",
+    answer: false,
+  },
+  {
+    question: '"div.my-div > h1#main-header" is a valid CSS selector',
+    answer: true,
+  },
+  {
+    question: "HTML 3 is the current version of HTML",
+    answer: false,
+  },
+  {
+    question: "Java is anaother name for JavaScript",
+    answer: false,
+  },
+  {
+    question: "JavaScript is a programming language",
+    answer: true,
+  },
+  {
+    question: "myNumber /= 4 is not the same us myNumber = myNumber/4",
+    answer: false,
+  },
+  {
+    question:
+      "With JS, I can remove or and add HTML and CSS dynamically to my site",
+    answer: true,
+  },
+];
