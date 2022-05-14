a24e8e511f519756e749567b751ef312ac1af6ee
hermitex <mercytibs@yahoo.com>
2022-05-14 16:20:55 +0300

    add video to result page

--- assets/js/result.js
+++ assets/js/result.js
@@ -1,10 +1,28 @@
-const resultContainer = document.querySelector('#score')
-
+const resultContainer = document.querySelector("#score");
 
 const scoreString = window.location.search;
 
 const scoreParams = new URLSearchParams(scoreString);
 
-const score = scoreParams.get('score')
+const score = scoreParams.get("score");
 
 resultContainer.textContent = score;
+
+// Get video container
+const videoContainer = document.querySelector(".results-firworks");
+
+// Video data
+const videos = [
+  {
+    url: "../assets/videos/fireworks-1.mp4",
+  },
+  {
+    url: "../assets/videos/fireworks-2.mp4",
+  },
+  {
+    url: "../assets/videos/fireworks-3.mp4",
+  },
+];
+
+let videoToPlay = Math.floor(Math.random() * videos.length);
+videoContainer.src = videos[videoToPlay].url;
